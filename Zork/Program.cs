81928079cd4b4201938276660599a0d42212d3dc
Mikaeyla <mikaeyla98@knights.ucf.edu>
2021-09-08 14:10:12 -0400

    Zork 1.2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -9,25 +9,10 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-
-            if (inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field West of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
-
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
         }
 
-
+        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
     }
 }
