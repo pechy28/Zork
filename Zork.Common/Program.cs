564bc5082547ae246891857a98ddb72fb0a78351
Mikaeyla <mikaeyla98@knights.ucf.edu>
2021-10-18 14:41:09 -0400

    Updates

---
+++ Zork.Common/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using System;
+
+
+namespace Zork
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            const string defaultGameFilename = "Zork.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
+
+            Game game = Game.Load(gameFilename);
+            Console.WriteLine("Welcome to Zork!");
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
+        }
+
+        private enum CommandLineArguments
+        {
+            GameFilename = 0
+        }
+    }
+}
+
+
+
+
+
+
+
