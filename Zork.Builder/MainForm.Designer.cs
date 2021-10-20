1a5606a541d7dea49166db7b376ea70a2a1f9bdc
Mikaeyla <mikaeyla98@knights.ucf.edu>
2021-10-20 15:15:49 -0400

    Updates: AGAIN

--- Zork.Builder/MainForm.Designer.cs
+++ Zork.Builder/MainForm.Designer.cs
@@ -2,7 +2,7 @@
 
 namespace Zork.Builder
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,10 +30,19 @@ namespace Zork.Builder
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
